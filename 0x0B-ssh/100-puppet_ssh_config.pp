998a5c757ddea6ce44367d8dc7867ead05859474
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-27 12:45:02 -0800

    Added 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Practice with puppet
+
+file { etc/ssh/ssh_config':
+	ensure => present,
+	content =>"
+	#SSH client config
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthenticate no",
+}
