e661afb0abf7da55bf13d3ae6887fced9d43044a
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 04:51:03 -0800

    Json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+2. Export to JSON
+"""
+import json
+import requests
+import sys
+
+
+def get_user_info(usr_id):
+    """
+    Requests for all info related to user of id
+    <usr_id>
+    """
+    url = "https://jsonplaceholder.typicode.com/users/{}"
+    url = url.format(usr_id)
+    response = requests.get(url)
+    return response.json()
+
+
+def get_todos(usr_id=None):
+    """
+    Returns a list of all todos
+    If <usr_id> is not None filters todos
+    for only those with provided use id
+    """
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    if usr_id is None:
+        return response.json()
+    filteredlist = [x for x in response.json()
+                    if str(x.get("userId", None)) == usr_id]
+    return filteredlist
+
+
+def export_to_json(data, filename):
+    """
+    Write <data> to <filename>.json
+    """
+    filename = str(filename)
+    with open(filename + ".json", 'w', encoding='UTF8') as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        usr = get_user_info(sys.argv[1])
+        if usr.get("name", None) is not None:
+            usr_todos = get_todos(sys.argv[1])
+            data = {sys.argv[1]: []}
+            for i in usr_todos:
+                row = {}
+                row["task"] = i.get("title")
+                row["completed"] = i.get("completed")
+                row["username"] = usr.get("username")
+                data[sys.argv[1]].append(row)
+            export_to_json(data, usr.get("id"))
