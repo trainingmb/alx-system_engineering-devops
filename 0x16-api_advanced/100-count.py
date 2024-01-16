63929bc1ead0b2da55aeb79795d4cdf01b40fa42
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-17 01:19:18 +0300

    Update 100-count.py
    Sort

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -63,6 +63,9 @@ def count_words(subreddit, word_list):
         for i in range(len(searches)):
             o = re.findall(searches[i], title)
             results[word_list[i]] += len(o)
-    for key, value in results.items():
+    results = sorted(results.items(),
+                     key=lambda x: x[1],
+                     reverse=True) 
+    for key, value in results:
         print("{}: {}".format(key, value))
     
