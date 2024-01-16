39d2c1ad9fab9a5860a33f00d300fe253beae573
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-16 02:16:02 -0800

    Added 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0. How many Subs?
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers in a
+    Subreddit
+    If the subreddit is not valid returns 0
+    """
+    headers = {'User-Agent': 'About Page : SubscriberCount /Reddit'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.headers.get('content-type', '') == \
+       'application/json':
+        return response.json()['data'].get("subscribers", 0)
+    return 0
