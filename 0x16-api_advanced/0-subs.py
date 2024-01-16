0ce66c3f6209f3cf92838228bef9689a16748bb3
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-16 06:21:14 -0800

    Added 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'About Page : SubscriberCount /Reddit'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers)
-    if response.headers.get('content-type', '') == \
-       'application/json':
+    if 'application/json' in \
+       response.headers.get('content-type', ''):
         return response.json()['data'].get("subscribers", 0)
     return 0
