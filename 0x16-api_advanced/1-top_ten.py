66e468fadd524adce697500ac71de793e2aa6862
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-16 06:20:17 -0800

    Added 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+1. Top Ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Quearies Reddit API and prints the titles of
+    the first 10 hot posts listed in the given subreddit
+    """
+    headers = {'User-Agent': 'Hot Top ten /Reddit'}
+    payload = {'count': '10',
+               'limit': '25'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=payload)
+    if 'application/json' in \
+       response.headers.get('content-type', ''):
+        data = response.json()['data']['children']
+        for child in data:
+            print(child.get('data',{}).get('title'))
+    else:
+        print(None)
